db97c6ca40cab34229ec4c0024eeaeaf18cfd4f1
Weronika Adamczyk <weronika.mroziak@gmail.com>
2023-02-18 22:59:30 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+list= {
+    "piekarnia" : ["chleb", "paczek", "bułki"],
+    "warzywniak" : ["marchew", "seler", "rukola"]
+}
+print("Lista zakupów")
+
+for shop in list:
+    print(f"Ide do {shop.capitalize()}", "i kupuję tam:", (list[shop]))
+    
+print(f'W sumie kupuję :{len(list["piekarnia"])+len(list["warzywniak"])} produktów')
